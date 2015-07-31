091a620dadc23d976bb2571977ec4f76fd31c0c7
sub77 <sub77@ymail.com>
2015-07-31 20:32:27 +0200

    usb: gadget lun file

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,7 +86,7 @@ MALLOC_IMPL := dlmalloc
 # Partitions and Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8226
